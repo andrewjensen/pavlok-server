d8c3125771e48d7537dc4f541743824252b07000
Lucas Crane <lucas.crane@domo.com>
2016-07-19 21:10:00 -0600

    actually add zap.js

---
+++ src/zap.js
@@ -0,0 +1,46 @@
+var pavlok = require('pavlok-beta-api-login');
+
+pavlok.init(
+    '942bf977af18c6104bfb6985ea7c13d61dcdf55ea7532ed292aba2d77095134f',
+    '302bb409e9275b005aafd9a5c088e2c2f5d025373941389860d97f3b88eaf51e',
+    {
+      port: 3001
+    }
+  );
+
+pavlok.login(function(result, code) {
+  if (result) {
+    console.log(result, code);
+  }
+  else {
+    console.log('could not log in');
+  }
+});
+
+let continueShock = false;
+
+function shock() {
+  console.log('try shock');
+  pavlok.zap({
+    value: 5,
+  });
+  if (continueShock) {
+    setTimeout(shock, 1000);
+  }
+}
+
+function start() {
+  continueShock = true;
+  shock();
+}
+
+function stop() {
+  continueShock = false;
+}
+
+module.exports = {
+  start,
+  stop
+};
+
+
